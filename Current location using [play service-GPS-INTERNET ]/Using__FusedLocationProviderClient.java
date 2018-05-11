3463dbac0adea7affa55e7f2581c0d0991802721
KUNAL KHEDKAR <kunalvkhedkar@gmail.com>
2018-05-11 17:57:29 +0530

    Using FusedLocationProviderClient New

--- Current location using [play service-GPS-INTERNET ]/Using__FusedLocationProviderClient.java
+++ Current location using [play service-GPS-INTERNET ]/Using__FusedLocationProviderClient.java
@@ -17,7 +17,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
+import android.view.View; 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
